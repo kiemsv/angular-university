78fb3b2f53d9db273899a51e767d8b397e6a047a
huy-kiem <65598444+kiemsv@users.noreply.github.com>
2022-10-18 11:05:22 +0700

    first commit

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/test-feature'),
+      dir: require('path').join(__dirname, './coverage/angular-university'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+@Component( {
+	selector    : 'app-root' ,
+	templateUrl : './app.component.html' ,
+	styleUrls   : [ './app.component.css' ]
+} )
 export class AppComponent {
-  title = 'test-feature';
+
+	testConcat() {
+		console.log( '---------------testConcat------------------' );
+		const series1$ = interval( 1000 ).pipe( map( val => 's1:' + ( val * 10 ).toString( 10 ) ) , takeWhile( v => parseInt( v.replace( 's1:' , '' ) , 10 ) <= 50 ) );
+		const series2$ = interval( 1000 ).pipe( map( val => 's2:' + ( val * 100 ).toString( 10 ) ) , takeWhile( v => parseInt( v.replace( 's2:' , '' ) , 10 ) <= 1000 ) );
+		const result$  = concat( series1$ , series2$ );
+		result$.subscribe( console.log );
+	}
+
+	testMap() {
+		console.log( '---------------testMap------------------' );
+		const ob$ = of( 'ob#1' , 'ob#2' , 'ob#3' , 'ob#4' , 'ob#5' );
+		ob$.pipe( map( value => value + ' : emitted' ) ).subscribe( console.log );
+	}
+
+	testConcatMap() {
+		console.log( '---------------testConcatMap------------------' );
+		const ob$ = of( 'ob#1' , 'ob#2' , 'ob#3' , 'ob#4' , 'ob#5' );
+		ob$.pipe( concatMap( value => of( value + ' : emitted' ) ) ).subscribe( console.log );
+	}
+
+	testMerge() {
+		console.log( '---------------testMerge------------------' );
+		const series1$ = interval( 1000 ).pipe( map( val => 's1:' + ( val * 10 ).toString( 10 ) ) , takeWhile( v => parseInt( v.replace( 's1:' , '' ) , 10 ) <= 50 ) );
+		const series2$ = interval( 1000 ).pipe( map( val => 's2:' + ( val * 100 ).toString( 10 ) ) , takeWhile( v => parseInt( v.replace( 's2:' , '' ) , 10 ) <= 1000 ) );
+		const result$  = merge( series1$ , series2$ );
+
+		result$.subscribe( console.log );
+	}
+
+	testMergeMap() {
+		console.log( '---------------testMergeMap------------------' );
+		const series1$ = interval( 1000 ).pipe( map( val => 's1:' + ( val * 10 ).toString( 10 ) ) , takeWhile( v => parseInt( v.replace( 's1:' , '' ) , 10 ) <= 50 ) );
+		const series2$ = interval( 1000 ).pipe( map( val => 's2:' + ( val * 100 ).toString( 10 ) ) , takeWhile( v => parseInt( v.replace( 's2:' , '' ) , 10 ) <= 1000 ) );
+		const result$  = merge( series1$ , series2$ );
+		result$.pipe( mergeMap( value => of( value + ' : emitted' ) ) ).subscribe( console.log );
+	}
 }
